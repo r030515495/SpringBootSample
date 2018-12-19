560ba2221b816ece5f24e9bcb1937ec48db2bb02
RexLi <r030515495@gmail.com>
2018-12-20 00:34:06 +0800

    add ApplicationPidFileWriter

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,14 +3,16 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.system.ApplicationPidFileWriter;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = { "com.example.controller" })
-@EntityScan({ "com.example.bean" })
+@SpringBootApplication(scanBasePackages = {"com.example.controller"})
+@EntityScan({"com.example.bean"})
 @EnableJpaRepositories("com.example.dao")
 public class DemoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication springApplication = new SpringApplication(DemoApplication.class);
+        springApplication.addListeners(new ApplicationPidFileWriter());
+        springApplication.run(args);
+    }
 }
